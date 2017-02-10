3f415328497c23f3bba613b5ed3a54ec0beaf7dc
Megaju <megaju@kijewoku.fr>
2017-02-10 13:06:34 +0100

    reorganization of the controller

--- js/angular-script.js
+++ js/angular-script.js
@@ -6,22 +6,15 @@ crudApp.controller("DbController",['$scope','$http', function($scope,$http){
     // Site title
     $scope.sitetitle = "AngularJS CRUD Operations Demo";
 
-    // READ : Function to get employee details from the database
-    getInfo();
-    function getInfo(){
-        // Sending request to EmpDetails.php files
-        $http.get('databaseFiles/empDetails.php').then(function(response){
-            // Stored the returned data into scope
-            $scope.details = response.data;
-        });
-    }
-
     // Display/Hidden form
     $scope.formToggle = function(){
         $('#empForm').toggleClass('hidden');
     }
 
-    // CREATE : Function to post employee details to the database
+    // ==================================================================
+    // ============================== CRUD ==============================
+    // ==================================================================
+    // CREATE
     $scope.insertInfo = function(info){
     $http.post('databaseFiles/insertDetails.php',
         {
@@ -35,5 +28,20 @@ crudApp.controller("DbController",['$scope','$http', function($scope,$http){
         });
     }
 
-    // end
+    // READ
+    getInfo();
+    function getInfo(){
+        // Sending request to EmpDetails.php files
+        $http.get('databaseFiles/empDetails.php').then(function(response){
+            // Stored the returned data into scope
+            $scope.details = response.data;
+        });
+    }
+
+    // UPDATE
+    
+
+    // DELETE
+
+    // the end :3
 }]);
